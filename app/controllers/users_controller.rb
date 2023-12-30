c54f84fdd9add90b5b3babe597197989020797a9
Kanae-Nishina <karubona.ra247@gmail.com>
2023-12-30 10:41:26 +0900

    ユーザー新規作成・ログイン機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user.email,@user.password)
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :nickname, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :first_name, presence: true, length: { maximum: 255 }
+  validates :nickname, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
