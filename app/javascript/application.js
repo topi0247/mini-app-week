3ed40f176910956a4a2bad4bd1259e90a5fe7f28
Kanae-Nishina <karubona.ra247@gmail.com>
2023-12-31 22:56:24 +0900

    投稿画像とアバターのプレビュー表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(function(){
+  $(document).on("change","#post_photo",function(){
+    let elem = this;
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(elem.files[0]);
+    fileReader.onload = (function () {
+        let imgTag = `<img src='${fileReader.result}'>`
+        $("#photo-prev").html(imgTag);
+    });
+  })
+
+  $(document).on("change","#user_avatar",function(){
+    let elem = this;
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(elem.files[0]);
+    fileReader.onload = (function () {
+        $('#avatar_prev').attr('src',fileReader.result);
+    });
+  })
+})
